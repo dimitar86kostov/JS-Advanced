9ed58b44a4dffab8890d6423b7e36f3bd7117f50
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-12-21 23:05:10 +0200

    Add files via upload

---
+++ arraysAndNestedArrays/11-equalNeighbors.js
@@ -0,0 +1,19 @@
+function equalNeighbors(array) {
+
+for (let i = 0; i < array.length; i++) {
+    let elArr = array[i];
+    
+for (let j = 0; j < elArr.length; j++) {
+    let element = array[j];
+    
+    
+}
+}
+
+}
+equalNeighbors([
+    ['2', '3', '4', '7', '0'],
+    ['4', '0', '5', '3', '4'],
+    ['2', '3', '5', '4', '2'],
+    ['9', '8', '7', '5', '4']
+])
