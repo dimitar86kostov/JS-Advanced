fc15cff0052f5f23e14f16397acc6792c68d132d
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-12-23 22:50:36 +0200

    Delete arraysAndNestedArrays directory

--- arraysAndNestedArrays/Exercises/10-ticTacToe.js
+++
@@ -1,6 +0,0 @@
-function ticTacToe(params) {
-    
-
-
-}
-ticTacToe()
--- arraysAndNestedArrays/Lab/05-smallestTwoNumbers.js
+++
@@ -1,9 +0,0 @@
-function smallestTwoNumbers(params) {
-
-
-let sorted = params.sort((a,b) => a - b);
-
-console.log(sorted[0],sorted[1]);
-
-}
-smallestTwoNumbers([30, 15, 50, 5] )
